fd483fc148f0b8c46f68c30d82c03a9ffbc0f021
Wang Zhen <wangzhen@xinri.com>
2019-08-12 00:28:29 +0800

    lee

--- src/main/java/com/mw/Study/Leetcode/Algorithm/a.java
+++ src/main/java/com/mw/Study/Leetcode/Algorithm/a.java
@@ -185,9 +185,27 @@ public class a {
         return (int)i;
     }
 
+    public static int reverse1(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+
+
 
     public static void main(String[] args) {
-        reverse(1534236469);
+        int i = -2147483648;
+        reverse1(i);
+        i=-i;
+        System.out.println(i);
+//        reverse(-2147483648);
 //        int[] v1 = {2,4,3};
 //        int[] v2 = {5,6,4};
 //        ListNode l1 = getListNodeFromArray(v1);
